04f645d114690fa2bb421a29672ea35ec4eb4b85
proedgeproedgeconsulting <proedgeconsultanting@gmail.com>
2025-03-09 16:48:32 -0400

    more simulators with correct url

--- src/data/courses/level1/quantumBasics/module6.ts
+++ src/data/courses/level1/quantumBasics/module6.ts
@@ -36,7 +36,7 @@ export const module6: Module = {
       points: 20,
       externalSimulator: {
         type: "iframe",
-        url: "/simulators/quantum_cryptography.html",
+        url: "public/simulators/quantum_cryptography.html",
         height: 600,
         width: "100%"
       }
